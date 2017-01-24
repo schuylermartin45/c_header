c8bf1f1e7bd4db0271ebc45bcb3c236b4755bf8d
Schuyler Martin <schuylermartin45@gmail.com>
2017-01-23 21:36:46 -0500

    Started to add C++ headers

---
+++ cpp/header.cpp
@@ -0,0 +1,31 @@
+/*
+** File:    TODO.cpp
+**
+** Author:  Schuyler Martin <sam8050@rit.edu>
+**
+** Description: TODO
+*/
+#include "TODO_header.h"
+
+// Namespaces
+using namespace std;
+
+/*
+** TODO expl
+**
+** @param name expl
+** @return
+*/
+void _intern_func()
+{
+}
+
+/*
+** TODO expl
+**
+** @param name expl
+** @return
+*/
+void extern_func()
+{
+}
---
+++ cpp/main.cpp
@@ -0,0 +1,21 @@
+/*
+** File:    main.cpp
+**
+** Author:  Schuyler Martin <sam8050@rit.edu>
+**
+** Description: TODO
+*/
+
+// Namespaces
+using namespace std;
+
+/*
+** Main execution point of the program
+**
+** @param argc Argument count
+** @param argv Argument list
+*/
+int main(int argc, char** argv )
+{
+    return 0;
+}
